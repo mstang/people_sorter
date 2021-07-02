3a3dbecb290c5a8438ec6aa93d79773055b15a73
Mark Stang <markjstang@gmail.com>
2021-07-02 15:13:12 -0600

    Refactoring delegates

--- lib/people_sorter.ex
+++ lib/people_sorter.ex
@@ -3,17 +3,27 @@ defmodule PeopleSorter do
   People Sorter takes in a list of Persons and returns them sorted.
   """
 
+  defdelegate add_person(person), to: PeopleSorter.PeopleList, as: :add_person_to_list
+  defdelegate add_person(pid, person), to: PeopleSorter.PeopleList, as: :add_person_to_list
+
+  defdelegate get_list(), to: PeopleSorter.PeopleList, as: :list
   defdelegate get_list(pid), to: PeopleSorter.PeopleList, as: :list
+  defdelegate get_list_sorted_by_dob(), to: PeopleSorter.PeopleList, as: :list_sorted_by_dob
   defdelegate get_list_sorted_by_dob(pid), to: PeopleSorter.PeopleList, as: :list_sorted_by_dob
+
+  defdelegate get_list_sorted_by_last_name(),
+    to: PeopleSorter.PeopleList,
+    as: :list_sorted_by_last_name
 
   defdelegate get_list_sorted_by_last_name(pid),
     to: PeopleSorter.PeopleList,
     as: :list_sorted_by_last_name
 
-  defdelegate get_list_sorted_by_color_last_name(pid),
+  defdelegate get_list_sorted_by_color_last_name(),
     to: PeopleSorter.PeopleList,
     as: :list_sorted_by_color_last_name
 
-  defdelegate add_person(person), to: PeopleSorter.PeopleList, as: :add_person_to_list
-  defdelegate add_person(pid, person), to: PeopleSorter.PeopleList, as: :add_person_to_list
+  defdelegate get_list_sorted_by_color_last_name(pid),
+    to: PeopleSorter.PeopleList,
+    as: :list_sorted_by_color_last_name
 end
