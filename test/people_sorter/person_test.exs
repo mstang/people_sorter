5788f9fcfb38ff2737948454bb0d022c8bda540a
Mark Stang <markjstang@gmail.com>
2021-07-03 14:24:06 -0600

    add more tests

--- test/people_sorter/person_test.exs
+++ test/people_sorter/person_test.exs
@@ -16,12 +16,40 @@ defmodule PeopleSorter.PersonTest do
              new_person
   end
 
+  test "don't create person with bad dob - e4/4/1930" do
+    parsed = Person.parse_person_line("Bauch Elliott luisa.kunde@schmeler.name Green e4/4/1930")
+
+    assert nil == Person.new(parsed)
+  end
+
+  test "don't create person with bad dob - 24/4/1930" do
+    parsed = Person.parse_person_line("Bauch Elliott luisa.kunde@schmeler.name Green 24/4/1930")
+
+    assert nil == Person.new(parsed)
+  end
+
+  test "create person from list" do
+    parsed = Person.parse_person_line("Bauch Elliott luisa.kunde@schmeler.name Green 4/4/1930")
+
+    assert %PeopleSorter.Person{
+             date_of_birth: ~D[1930-04-04],
+             email: "luisa.kunde@schmeler.name",
+             favorite_color: "Green",
+             first_name: "Elliott",
+             last_name: "Bauch"
+           } == Person.new(parsed)
+  end
+
   test "convert m/d/year to Date" do
-    assert ~D[1902-01-22] == Person.convert_date_to_dob("01/22/1902")
+    assert {:ok, ~D[1902-01-22]} == Person.convert_date_to_dob("01/22/1902")
+  end
+
+  test "convert alphanumeric dob to Date" do
+    assert {:error, :invalid_date} == Person.convert_date_to_dob("e1/22/1902")
   end
 
   test "test bad Date" do
-    assert :invalid_date = Person.convert_date_to_dob("22/01/1902")
+    assert {:error, :invalid_date} = Person.convert_date_to_dob("22/01/1902")
   end
 
   test "to string, test string formatting" do
